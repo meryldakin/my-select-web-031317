251716855b5ba73724711cac69f18148224fd280
Meryl Dakin <meryl.k.dakin@gmail.com>
2017-03-04 18:11:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  results = []
+  while i < collection.length
+    if yield(collection[i]) == true
+      results.push(collection[i])
+    end
+      i += 1
+  end
+  results
 end
